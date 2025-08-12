e56dc3012ca2d1f9338bb3bc307a11809ecdce59
FruitfulEjiro <frutfl@outlook.com>
2025-08-13 00:36:48 +0100

    cors fix

--- controller/authController.js
+++ controller/authController.js
@@ -17,14 +17,17 @@ const generateToken = userID => {
 
 const createSendToken = (user, code, res) => {
   const token = generateToken(user._id);
+  const isProduction = process.env.NODE_ENV === "production";
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: true,
+    secure: isProduction, 
+    sameSite: isProduction ? "none" : "lax",
   };
 
-  if (process.env.NODE_ENV === "Production") cookieOptions.secure = true;
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
   res.cookie("jwt", token, cookieOptions).json({
     status: "Success",
